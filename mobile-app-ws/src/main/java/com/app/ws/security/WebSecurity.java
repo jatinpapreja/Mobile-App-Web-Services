7ee28aa02400a56ba57f271f45fda95c649f2404
Jatin Papreja <jatinpapreja@gmail.com>
2022-09-18 19:36:31 +0530

    added authentication for sign in

--- mobile-app-ws/src/main/java/com/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/app/ws/security/WebSecurity.java
@@ -2,16 +2,18 @@ package com.app.ws.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.app.ws.service.UserService;
 
 @EnableWebSecurity
-public class WebSecurity{
+public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
 	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -21,26 +23,42 @@ public class WebSecurity{
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 	
-	@Bean
-	protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
-		
-		// Configure AuthenticationManagerBuilder
-		AuthenticationManagerBuilder authenticationManagerBuilder =
-				http.getSharedObject(AuthenticationManagerBuilder.class);
-		
-		authenticationManagerBuilder
-		.userDetailsService(userDetailsService)
-		.passwordEncoder(bCryptPasswordEncoder);
-		
-		
+//	@Bean
+//	protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+//		
+//		// Configure AuthenticationManagerBuilder
+//		AuthenticationManagerBuilder authenticationManagerBuilder =
+//				http.getSharedObject(AuthenticationManagerBuilder.class);
+//		
+//		authenticationManagerBuilder
+//		.userDetailsService(userDetailsService)
+//		.passwordEncoder(bCryptPasswordEncoder);
+//		
+//		
+//		http.csrf().disable()
+//		.authorizeRequests()
+//		.antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL)
+//		.permitAll()
+//		.anyRequest().authenticated().and()
+//		.addFilter(new AuthenticationFilter(authenticationManager()));
+//		
+//		return http.build();
+//	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable()
 		.authorizeRequests()
-		.antMatchers(HttpMethod.POST,"/users")
+		.antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL)
 		.permitAll()
-		.anyRequest().authenticated();
+		.anyRequest().authenticated().and()
+		.addFilter(new AuthenticationFilter(authenticationManager()));
 		
-		return http.build();
 	}
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
 
 }
