991ac4ca033797a2e08c9bee70764e5ad64d5af0
Jatin Papreja <jatinpapreja@gmail.com>
2022-09-19 20:44:15 +0530

    customized user authentication url

--- mobile-app-ws/src/main/java/com/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/app/ws/security/WebSecurity.java
@@ -52,13 +52,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL)
 		.permitAll()
 		.anyRequest().authenticated().and()
-		.addFilter(new AuthenticationFilter(authenticationManager()));
+		.addFilter(getAuthenticationFilter());
 		
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	public AuthenticationFilter getAuthenticationFilter() throws Exception{
+		
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
+		
 	}
 
 }
