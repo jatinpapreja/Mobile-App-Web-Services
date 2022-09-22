6cbf7d67350a72406c3f020b87466d289e6dc818
Jatin Papreja <jatinpapreja@gmail.com>
2022-09-22 22:42:00 +0530

    read token secret from a property file

--- mobile-app-ws/src/main/java/com/app/ws/security/SecurityConstants.java
+++ mobile-app-ws/src/main/java/com/app/ws/security/SecurityConstants.java
@@ -1,11 +1,18 @@
 package com.app.ws.security;
 
+import com.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 	
 	public static final long EXPIRATION_TIME = 864000000;  // 10 Days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+	public static final String TOKEN_SECRET = getTokenSecret();
+	
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties)SpringApplicationContext.getBean("AppProperties");
+		return appProperties.getTokenSecret();
+	}
 
 }
