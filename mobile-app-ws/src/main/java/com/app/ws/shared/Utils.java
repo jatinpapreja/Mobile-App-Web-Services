32a22ad59512f8389c2d8c0398484aed48643fe6
Jatin Papreja <jatinpapreja@gmail.com>
2022-10-08 15:26:32 +0530

    created JUnit Integration Testcase

--- mobile-app-ws/src/main/java/com/app/ws/shared/Utils.java
+++ mobile-app-ws/src/main/java/com/app/ws/shared/Utils.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import com.app.ws.security.SecurityConstants;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -40,16 +41,24 @@ public class Utils {
 	}
 
 	public static boolean hasTokenExpired(String token) {
-		Claims claims = Jwts.parser()
-				.setSigningKey(SecurityConstants.TOKEN_SECRET)
-				.parseClaimsJws(token).getBody();
+		boolean returnValue=false;
 		
+		try {
+			Claims claims = Jwts.parser()
+					.setSigningKey(SecurityConstants.TOKEN_SECRET)
+					.parseClaimsJws(token).getBody();
+			
+			
+			Date tokenExpirationDate = claims.getExpiration();
+			Date todayDate = new Date();
+			
+			returnValue = tokenExpirationDate.before(todayDate);
+		}
+		catch(ExpiredJwtException ex) {
+			returnValue = true;
+		}
 		
-		Date tokenExpirationDate = claims.getExpiration();
-		Date todayDate = new Date();
-		
-		
-		return tokenExpirationDate.before(todayDate);
+		return returnValue;
 	}
 
 	public String generateEmailVerificationToken(String userId) {
--- mobile-app-ws/src/test/java/com/app/ws/shared/UtilsTest.java
+++ mobile-app-ws/src/test/java/com/app/ws/shared/UtilsTest.java
@@ -3,9 +3,16 @@ package com.app.ws.shared;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class UtilsTest {
+	
+	@Autowired
+	Utils utils;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -13,7 +20,38 @@ class UtilsTest {
 
 	@Test
 	void testGenerateUserId() {
-		fail("Not yet implemented");
+		String userId = utils.generateUserId(30);
+		String userId2 = utils.generateUserId(30);
+
+		
+		assertNotNull(userId);
+		assertNotNull(userId2);
+		assertTrue(userId.length()==30);
+		assertTrue(!userId.equalsIgnoreCase(userId2));
+		
+	}
+	
+	@Test
+	void testTokenNotExpired() {
+		
+		String token = utils.generateEmailVerificationToken("34jkdfk3kh345k");
+		assertNotNull(token);
+		
+		boolean hasTokenExpired = Utils.hasTokenExpired(token);
+		
+		assertFalse(hasTokenExpired);
+	}
+	
+	@Test
+//	@Disabled
+	void testTokenExpired() {
+		
+		String expiredToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QDEyMy5jb20iLCJleHAiOjE2NjQ3NzI1OTh9.g5ln0A-f7oVyi58tmFbGSBsKb661W7CrjnwDKver5WxNaJqcGEjFuOOx3vyE067adnKoq05GeAwWClJ4uKnN_Q";
+		boolean hasTokenExpired = Utils.hasTokenExpired(expiredToken);
+		
+		assertTrue(hasTokenExpired);
+
+
 	}
 
 }
