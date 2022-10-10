33aaf2db9d2a0d17c85c2a8081ae21a6b454ccf1
Jatin Papreja <jatinpapreja@gmail.com>
2022-10-10 11:03:26 +0530

    created sql queries and tested them

--- mobile-app-ws/src/main/java/com/app/ws/io/repositories/UserRepository.java
+++ mobile-app-ws/src/main/java/com/app/ws/io/repositories/UserRepository.java
@@ -1,7 +1,13 @@
 package com.app.ws.io.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.ws.io.entity.UserEntity;
@@ -11,4 +17,18 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
 	UserEntity findUserByEmailVerificationToken(String token);
+	
+	@Query(value="select * from Users u where u.EMAIL_VERIFICATION_STATUS=TRUE",
+			countQuery="select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS=TRUE",
+			nativeQuery=true)
+	Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
+	
+	
+	@Query(value="select * from Users u where u.first_name=?1",
+			nativeQuery=true)
+	List<UserEntity> findUserByFirstName(String firstName);
+	
+	@Query(value="select * from Users u where u.last_name=:lastName",
+			nativeQuery=true)
+	List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
 }
---
+++ mobile-app-ws/src/test/java/com/app/ws/io/repository/UserRepositoryTest.java
@@ -0,0 +1,138 @@
+package com.app.ws.io.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.app.ws.io.entity.AddressEntity;
+import com.app.ws.io.entity.UserEntity;
+import com.app.ws.io.repositories.UserRepository;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+	@Autowired
+	UserRepository userRepository;
+
+	static boolean recordsCreated = false;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		if (!recordsCreated)
+			createRecords();
+	}
+
+	@Test
+	void testGetVerifiedUsers() {
+		Pageable pageableRequest = PageRequest.of(0, 2);
+		Page<UserEntity> page = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest);
+		assertNotNull(page);
+
+		List<UserEntity> userEntities = page.getContent();
+		assertNotNull(userEntities);
+		assertTrue(userEntities.size() == 2);
+
+		Pageable pageableRequest2 = PageRequest.of(0, 1);
+		Page<UserEntity> page2 = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest2);
+		assertNotNull(page2);
+
+		List<UserEntity> userEntities2 = page2.getContent();
+		assertNotNull(userEntities2);
+		assertTrue(userEntities2.size() == 1);
+
+		Pageable pageableRequest3 = PageRequest.of(1, 1);
+		Page<UserEntity> page3 = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest3);
+		assertNotNull(page3);
+
+		List<UserEntity> userEntities3 = page3.getContent();
+		assertNotNull(userEntities3);
+		assertTrue(userEntities3.size() == 1);
+	}
+
+	@Test
+	void testFindUserByFirstName() {
+		String firstName = "Jatin";
+		List<UserEntity> users = userRepository.findUserByFirstName(firstName);
+		assertNotNull(users);
+		assertTrue(users.size() == 2);
+
+		UserEntity user = users.get(0);
+		assertEquals(firstName, user.getFirstName());
+	}
+
+	@Test
+	void testFindUserByLastName() {
+		String lastName = "Papreja";
+		List<UserEntity> users = userRepository.findUserByLastName(lastName);
+		assertNotNull(users);
+		assertTrue(users.size() == 2);
+
+		UserEntity user = users.get(0);
+		assertEquals(lastName, user.getLastName());
+	}
+
+	private void createRecords() {
+		// Prepare User Entity
+		UserEntity userEntity = new UserEntity();
+		userEntity.setFirstName("Jatin");
+		userEntity.setLastName("Papreja");
+		userEntity.setEmail("test@test.com");
+		userEntity.setUserId("1a2b3c");
+		userEntity.setEncryptedPassword("xxx");
+		userEntity.setEmailVerificationStatus(true);
+
+		// Prepare User Addresses
+		AddressEntity addressEntity = new AddressEntity();
+		addressEntity.setType("shipping");
+		addressEntity.setAddressId("adk2k32kh2");
+		addressEntity.setCity("Delhi");
+		addressEntity.setCountry("India");
+		addressEntity.setStreetName("123 Street name");
+		addressEntity.setPostalCode("ABC123");
+
+		List<AddressEntity> addresses = new ArrayList<>();
+		addresses.add(addressEntity);
+
+		userEntity.setAddresses(addresses);
+
+		userRepository.save(userEntity);
+
+		// Prepare User Entity
+		UserEntity userEntity2 = new UserEntity();
+		userEntity2.setFirstName("Jatin");
+		userEntity2.setLastName("Papreja");
+		userEntity2.setEmail("test@test.com");
+		userEntity2.setUserId("1a2b3cdd");
+		userEntity2.setEncryptedPassword("xxx");
+		userEntity2.setEmailVerificationStatus(true);
+
+		// Prepare User Addresses
+		AddressEntity addressEntity2 = new AddressEntity();
+		addressEntity2.setType("shipping");
+		addressEntity2.setAddressId("adk2k32kh2dd");
+		addressEntity2.setCity("Delhi");
+		addressEntity2.setCountry("India");
+		addressEntity2.setStreetName("123 Street name");
+		addressEntity2.setPostalCode("ABC123");
+
+		List<AddressEntity> addresses2 = new ArrayList<>();
+		addresses2.add(addressEntity2);
+
+		userEntity2.setAddresses(addresses2);
+
+		userRepository.save(userEntity2);
+
+		recordsCreated = true;
+	}
+
+}
