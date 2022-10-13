65f9e9c5a3d570df925107a94a8d7675b37bb80f
Jatin Papreja <jatinpapreja@gmail.com>
2022-10-13 23:28:44 +0530

    added swagger documentation

--- mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import com.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/mobile-app-ws/users
+@CrossOrigin(origins="*")
 public class UserController {
 
 	@Autowired
